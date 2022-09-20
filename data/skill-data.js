213d18cd66cbf5c3d01b172e36a9639eca91879a
trmorningstar <trmorningstar@gmail.com>
2022-09-20 18:56:34 -0400

    refactor routing

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'CSS', done: true, _id: 483957},
+  {text: 'JavaScript', done: false, _id: 392854},
+  {text: 'HTML', done: true, _id: 302945},
+]
+
+export { 
+	skills
+}
